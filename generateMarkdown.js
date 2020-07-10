870cce96b6db75ff630059a563eec5f56bc6e9a1
Walter Wang <wsglobe@gmail.com>
2020-07-09 22:28:08 -0500

    added files

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,31 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+let index = require('./index.js');
 
+function generateMarkdown(data) {
+  console.log(data);
+  return `
+  <a href="https://github.com/${data.username}"><img src="https://img.shields.io/badge/Github%20page-${data.username}-1abc9c.svg" alt="User's github profile"></a>
+![alt text](https://github.com/${data.username}.png)
+  # Username:
+  ${data.username}
+  # Title:
+  ${data.title}
+  # Description:
+  ${data.description}
+  # Table of Contents;
+  ${data.toc}
+  # Installation:
+  ${data.installation}
+  # Usage:
+  ${data.usage}
+  # License:
+  ${data.license}
+  # Contribute:
+  ${data.contribute}
+  # Tests:
+  ${data.tests}
+  # Questions:
+  ${data.questions}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,16 +1,73 @@
-// array of questions for user
-const questions = [
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
 
-];
+// array of questions for user
+const questions = [{
+  type: "input",
+  name: "username",
+  message: "What is your GitHub user name?",
+},
+{
+  type: "input",
+  name: "title",
+  message: "What is your project title?",
+},
+{
+  type: "input",
+  name: "description",
+  message: "What is your project description?",
+},
+{
+  type: "input",
+  name: "toc",
+  message: "Enter Table of Contents to make it easier for users to find what they need",
+},
+{
+  type: "input",
+  name: "installation",
+  message: "What steps are required to install your project? Please provide instructions to setup development environment.",
+},
+{
+  type: "input",
+  name: "usage",
+  message: "Provide instructions and examples for use. Include screenshots as needed.",
+},
+{
+  type: "input",
+  name: "contribute",
+  message: "List collaborators and link their GitHub profiles, third-party assets, and any tutorials used.",
+},
+{
+  type: "input",
+  name: "tests",
+  message: "List any tests written and examples on how to run them.",
+},
+{
+  type: "input",
+  name: "license",
+  message: "List any licenses' your project may include. Choices include ISC, MIT, Apache License 2.0, GNU GPLv2, GNU GPLv3",
+},
+]
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+  fs.writeFile(fileName, data, "utf-8", function (err) {
+    if (err) throw err;
+    console.log("success")
+  });
+
+  // function to initialize program
+  function init() {
+    inquirer.prompt(questions).then(function (data) {
+      let readme = generateMarkdown(data);
+      const fileName = "README.md";
+      return writeToFile(fileName, readme)
+
+    })
+  }
+  // function call to initialize program
+  init();
+
 }
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
